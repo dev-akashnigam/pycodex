ec5522b59f6d8d560d9ddd86b1eb4e41d301636c
Akash Nigam <contact.akashnigam@gmail.com>
2025-07-19 12:34:54 +0530

    print hello world program

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+display_str = "Hello World!";
+
+print("First print statement : "+display_str)
+
+print(f"First print statement : {display_str}")
+
+print("First print statement : {}".format(display_str))
