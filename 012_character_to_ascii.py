903f877c42c7e03e7976359cad406bb0ee7c0d26
Akash Nigam <contact.akashnigam@gmail.com>
2025-07-24 05:57:18 +0530

    Adding programs for character to ascii interconversion

---
+++ 012_character_to_ascii.py
@@ -0,0 +1,7 @@
+print("Please enter a character : ")
+ch = input()[0]
+
+print(f"The character you have entered is : {ch}")
+
+ascii = ord(ch)
+print(f"The ascii value of character {ch} is {ascii}")
---
+++ 013_ascii_to_character.py
@@ -0,0 +1,7 @@
+print("Please enter a ASCII value")
+ascii = int(input())
+
+print(f"The ascii value you have entered is : {ascii}")
+
+ch = chr(ascii)
+print(f"The character corresponding to the ascii value {ascii} is : {ch}")
