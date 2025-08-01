962541463e7fd2e42e77023c4e80e449d97242f9
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 15:36:25 +0530

    Max and Min real and decimal numbers supported by Python

---
+++ 034_max_and_min_real_number.py
@@ -0,0 +1,7 @@
+import sys
+
+maxRealNo = sys.maxsize
+minRealNo = -sys.maxsize-1
+
+print(f"Max real number supported by Python = {maxRealNo}")
+print(f"Min real number supported by Python = {minRealNo}")
---
+++ 035_max_and_min_decimal_number.py
@@ -0,0 +1,7 @@
+import sys
+
+maxDecimalNo = sys.float_info.max
+minDecimalNo = -sys.float_info.max-1
+
+print(f"Max decimal number supported by Python = {maxDecimalNo}")
+print(f"Min decimal number supported by Python = {minDecimalNo}")
