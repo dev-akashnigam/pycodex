687deb0f169e1ca7c64934094034f13a9b668249
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 16:01:32 +0530

    Positive and Negative Infinity in Python

---
+++ 036_positive_and_negative_infinity.py
@@ -0,0 +1,7 @@
+import math
+
+POSITIVE_INFINITY = math.inf
+NEGATIVE_INFINITY = -math.inf
+
+print(f"Positive infinity in Python = {POSITIVE_INFINITY}")
+print(f"Negative infinity in Python = {NEGATIVE_INFINITY}")
