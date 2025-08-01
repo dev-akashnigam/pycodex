a79da533b8b6855063640d6c19767d39770c64e1
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-01 13:32:00 +0530

    Adding programs for calculating square and cube root in different ways

---
+++ 031_square_and_cube_root.py
@@ -0,0 +1,10 @@
+import math
+
+print("Please enter a number : ")
+NUM = float(input())
+
+SQUARE_ROOT = math.pow(NUM, (1.0/2.0))
+CUBE_ROOT = math.pow(NUM, (1.0/3.0))
+
+print(f"Square root of number:{NUM} = {SQUARE_ROOT}")
+print(f"Cube root of number:{NUM} = {CUBE_ROOT}")
---
+++ 032_square_and_cube_root_1.py
@@ -0,0 +1,10 @@
+import math
+
+print("Please enter a number : ")
+NUM = float(input())
+
+SQUARE_ROOT = math.sqrt(NUM)
+CUBE_ROOT = math.cbrt(NUM)
+
+print(f"Square root of number:{NUM} = {SQUARE_ROOT}")
+print(f"Cube root of number:{NUM} = {CUBE_ROOT}")
