416febb04f3b4c6511fd615056463d445b27745d
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-02 08:45:46 +0530

    Text to Number Conversion

---
+++ 039_text_to_number_conversion.py
@@ -0,0 +1,8 @@
+REAL_NO_TEXT = "1268"
+DECIMAL_NO_TEXT = "35.21549"
+
+REAL_NO_INTEGER = int(REAL_NO_TEXT)
+DECIMAL_NO_FLOAT = float(DECIMAL_NO_TEXT)
+
+print(f'Integer corresponding to text "{REAL_NO_TEXT}" = {REAL_NO_INTEGER}')
+print(f'Decimal Number corresponding to text "{DECIMAL_NO_TEXT}" = {DECIMAL_NO_FLOAT}')
