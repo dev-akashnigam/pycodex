cbd02126c4cf3d119f1c9bc464473e003216eb46
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-02 06:13:02 +0530

    Formatting decimal numbers to N places after decimal

---
+++ 037_formatting_decimal_numbers.py
@@ -0,0 +1,5 @@
+print("Please enter a decimal number : ")
+DECIMAL_NO = float(input())
+
+FORMATTED_DECIMAL_NO = f"{DECIMAL_NO:.3f}"
+print(f"Formatting decimal number {DECIMAL_NO} to 3 decimal places, the number becomes = {FORMATTED_DECIMAL_NO}")
