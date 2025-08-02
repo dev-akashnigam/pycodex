f0aa8ebca63bdd2e9a22c22476afbd803ddb2959
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-02 15:56:16 +0530

    Number to Number interconversion and text/string to boolean conversion

---
+++ 040_number_to_number_conversion.py
@@ -0,0 +1,8 @@
+REAL_NO = 1356
+DECIMAL_NO = 654.16854
+
+FLOAT_FROM_REAL_NUMBER = float(REAL_NO)
+INT_FROM_DECIMAL_NUMBER = int(DECIMAL_NO)
+
+print(f"Real Number {REAL_NO} converted to Decimal Number becomes = {FLOAT_FROM_REAL_NUMBER}")
+print(f"Decimal Number {DECIMAL_NO} converted to Real Number becomes = {INT_FROM_DECIMAL_NUMBER}")
---
+++ 041_text_to_boolean_conversion_placeholder.py
@@ -0,0 +1,4 @@
+# there is no direct way to convert a string/text to boolean in PY, same as JS
+# Method like bool(str) is not available in PY
+# Manually you have to accept user input, convert to lower case and then compare this lower case input with
+# values that represent boolean values like true/false, 0/1 etc.
