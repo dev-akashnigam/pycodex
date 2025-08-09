f5002257a1ad1d36f707ed96153876e142d03765
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-09 11:45:11 +0530

    Adding two if-else programs - finding greater of two numbers and finding if the number is greater than , lesser than or equal to 0

---
+++ 052_larger_of_two_different_numbers.py
@@ -0,0 +1,10 @@
+print("Please enter a number : ")
+my_first_num = float(input())
+
+print("Please enter another number : ")
+my_second_num = float(input())
+
+if my_first_num > my_second_num : 
+    print(f"{my_first_num} is greater than {my_second_num}")
+else : 
+    print(f"{my_second_num} is greater than {my_first_num}")
---
+++ 053_check_positive_negative_or_zero.py
@@ -0,0 +1,9 @@
+print("Please enter a number : ")
+my_num = float(input())
+
+if my_num>0 : 
+    print(f"{my_num} is greater than 0.")
+elif my_num==0 : 
+    print(f"{my_num} is equal to 0.")
+else:
+    print(f"{my_num} is less than 0.")
