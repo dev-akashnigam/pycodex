31d605b0ac2ba8765585c36f9cec968d41291775
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-13 06:16:56 +0530

    Adding two ternary operator programs - postive or negative number check and alphabet check

---
+++ 063_ternary_operator_positive_or_negative_num_check.py
@@ -0,0 +1,6 @@
+print("Please enter a real number: ")
+MY_NUM = int(input())
+
+OUTPUT_STRING = f"Number: {MY_NUM} is POSITIVE!" if MY_NUM>=0 else f"Number: {MY_NUM} is NEGATIVE!"
+
+print(OUTPUT_STRING)
