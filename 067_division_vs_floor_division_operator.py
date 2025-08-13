c3bf927c848805bdef7cd8797c273c101b06bda1
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-13 10:55:00 +0530

    Addding program show-casing difference b/w division and  floor division operator

---
+++ 067_division_vs_floor_division_operator.py
@@ -0,0 +1,8 @@
+dividend = 7
+divisor = 2
+
+divisionResult = dividend / divisor
+floorDivisionResult = dividend // divisor
+
+print(f"Dividend: {dividend} when divided by divisor: {divisor} = {divisionResult}")
+print(f"Dividend: {dividend} when floor divided by divisor: {divisor} = {floorDivisionResult}")
