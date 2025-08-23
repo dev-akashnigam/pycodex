d43aa028f3fd949dde1c171dfe1a638f8966beda
Akash Nigam <contact.akashnigam@gmail.com>
2025-08-23 13:11:49 +0530

    Print no of digits in a number using loop

---
+++ 088_count_digits_in_num.py
@@ -0,0 +1,11 @@
+import math
+
+print("Please enter a number: ")
+myNum = int(input())
+
+noOfDigits = 0
+while myNum!= 0:
+    noOfDigits += 1
+    myNum = math.floor(myNum / 10)
+
+print(f"Number of digits = {noOfDigits}")
